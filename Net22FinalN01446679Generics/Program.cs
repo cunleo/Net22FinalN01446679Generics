cad006ff4ab1f651c175442d6cd426e38c709ef5
OLAPOJU OLUWOLE <35400817+cunleo@users.noreply.github.com>
2020-08-14 21:43:47 -0400

    Blank Project

--- Net22FinalN01446679Generics/Program.cs
+++ Net22FinalN01446679Generics/Program.cs
@@ -6,7 +6,7 @@ namespace Net22FinalN01446679Generics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World");
         }
     }
 }
