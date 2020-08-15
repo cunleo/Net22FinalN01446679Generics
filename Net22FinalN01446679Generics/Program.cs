4cf04c67d2943d1332d00c6a4bb7d2d4eec338ee
OLAPOJU OLUWOLE <35400817+cunleo@users.noreply.github.com>
2020-08-14 21:42:53 -0400

    Add project files.

---
+++ Net22FinalN01446679Generics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Net22FinalN01446679Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
